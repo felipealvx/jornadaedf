1028d0e83860cd8203d17d2ff2aee0fbdedf95c9
felipealvx <felipealvk@gmai.com>
2024-10-12 01:38:14 -0300

    adição de novas coisas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
-const link = document.querySelectorAll("card-link")
+const btn = document.getElementById("programming")
+const content = document.querySelector(".content-programming")
 
-function show(){
-  
+function showContent(){
+  if (content.style.display === "block"){
+    content.style.display = "none"
+  } else {
+    content.style.display = "block"
+  }
 }
